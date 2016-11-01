16b9555d6a97e2f538d9b0b3c90a0af289504f84
chulchoice <chulchoicezee@gmail.com>
2016-11-01 16:07:01 +0900

    init

---
+++ src/decorator/Decorator.java
@@ -0,0 +1,15 @@
+package decorator;
+
+public class Decorator {
+
+	public Decorator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ems");
+	}
+	
+
+}
---
+++ src/decorator/Display.java
@@ -0,0 +1,9 @@
+package decorator;
+
+public abstract class Display {
+
+	public Display() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
